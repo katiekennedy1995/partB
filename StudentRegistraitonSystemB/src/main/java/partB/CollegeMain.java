8d8c37bfbc540a01e2cbe9f92352f465cc5003de
katiekennedy1995 <katiekennedy1995@users.noreply.github.com>
2017-10-12 15:51:49 +0100

    version 1

---
+++ StudentRegistraitonSystemB/src/main/java/partB/CollegeMain.java
@@ -0,0 +1,12 @@
+package partB;
+
+
+public class CollegeMain {
+
+	public static void main(String [ ] args)
+	{
+	     College college = new College();
+	     college.CollegeArrays();
+	     college.print();
+	}
+}
